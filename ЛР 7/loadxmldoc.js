0cf121b1ea497ba87ea589b81d6df5f436ed6771
Varvara <45296172+varvara19@users.noreply.github.com>
2018-11-23 21:46:02 +0300

    izmenila

--- ЛР 7/loadxmldoc.js
+++ ЛР 7/loadxmldoc.js
@@ -8,7 +8,7 @@ else
  {
  var xhttp=new ActiveXObject("Microsoft.XMLHTTP");
  }
-xhttp.open("GET",dname,false);
+
 xhttp.setRequestHeader("Content-Type", "text/xml");
 xhttp.send();
 return xhttp.responseXML;
